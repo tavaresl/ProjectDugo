f550907b9461c2af80d48bf0d23a572e0145e47a
Falme Streamless <falme_streamless@hotmail.com>
2020-08-29 13:57:06 -0300

    Created Life (Same as in the video)

--- ProjectDugo/ProjectDugo.cpp
+++ ProjectDugo/ProjectDugo.cpp
@@ -6,13 +6,60 @@
 
 class ProjectDugo_Game : public olcConsoleGameEngine {
 public:
+
+    ProjectDugo_Game()
+    {
+        m_sAppName = L"OLCJAM 2020 - GAME OF LIFE";
+    }
+
     virtual bool OnUserCreate() {
+
+        m_output = new int[ScreenWidth() * ScreenHeight()];
+        m_state = new int[ScreenWidth() * ScreenHeight()];
+        memset(m_output, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
+        memset(m_state, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
+
+        for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+            m_state[i] = rand() % 2;
+
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime) {
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        auto cell = [&](int x, int y)
+        {
+            return m_output[y * ScreenWidth() + x];
+        };
+
+        for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+            m_output[i] = m_state[i];
+
+        for(int x=1; x<ScreenWidth() -1; x++)
+            for (int y = 1; y < ScreenHeight() - 1; y++)
+            {
+                int nNeighbours =   cell(x - 1, y - 1) + cell(x, y - 1) + cell(x + 1, y - 1) +
+                                    cell(x - 1, y) +        0           + cell(x + 1, y) +
+                                    cell(x - 1, y + 1) + cell(x, y + 1) + cell(x + 1, y + 1);
+
+                if (cell(x, y) == 1)
+                    m_state[y * ScreenWidth() + x] = nNeighbours == 2 || nNeighbours == 3;
+                else
+                    m_state[y * ScreenWidth() + x] = nNeighbours == 3;
+
+                if (cell(x, y) == 1) Draw(x, y, PIXEL_SOLID, FG_WHITE);
+                else Draw(x, y, PIXEL_SOLID, FG_BLACK);
+
+            }
+
+
         return true;
     }
+private:
+    int* m_output;
+    int* m_state;
 };
 
 int main()
