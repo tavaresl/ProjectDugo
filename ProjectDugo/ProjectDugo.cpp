cc83c3d83dd996487aa2fd0a9082cb35b4866c32
Falme Streamless <falme_streamless@hotmail.com>
2020-08-29 15:21:57 -0300

    updated area of spawn

--- ProjectDugo/ProjectDugo.cpp
+++ ProjectDugo/ProjectDugo.cpp
@@ -1,15 +1,13 @@
-// ProjectDugo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <stdio.h>
 #include "olcConsoleGameEngine.h"
 
 class ProjectDugo_Game : public olcConsoleGameEngine {
 public:
 
-    ProjectDugo_Game()
+    ProjectDugo_Game(int _lifeArea)
     {
         m_sAppName = L"OLCJAM 2020 - GAME OF LIFE";
+        lifeArea = _lifeArea;
     }
 
     virtual bool OnUserCreate() {
@@ -19,8 +17,10 @@ public:
         memset(m_output, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
         memset(m_state, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
 
-        for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
-            m_state[i] = rand() % 2;
+        for (int x = 1; x < ScreenWidth() - 1; x++)
+            for (int y = 1; y < ScreenHeight() - 1; y++)
+                if(IsInLifeArea(x,y,lifeArea))
+                    m_state[y * ScreenWidth() + x] = rand() % 2;
 
         return true;
     }
@@ -37,12 +37,12 @@ public:
         for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
             m_output[i] = m_state[i];
 
-        for(int x=1; x<ScreenWidth() -1; x++)
+        for (int x = 1; x < ScreenWidth() - 1; x++)
             for (int y = 1; y < ScreenHeight() - 1; y++)
             {
-                int nNeighbours =   cell(x - 1, y - 1) + cell(x, y - 1) + cell(x + 1, y - 1) +
-                                    cell(x - 1, y) +        0           + cell(x + 1, y) +
-                                    cell(x - 1, y + 1) + cell(x, y + 1) + cell(x + 1, y + 1);
+                int nNeighbours = cell(x - 1, y - 1) + cell(x, y - 1) + cell(x + 1, y - 1) +
+                    cell(x - 1, y) + 0 + cell(x + 1, y) +
+                    cell(x - 1, y + 1) + cell(x, y + 1) + cell(x + 1, y + 1);
 
                 if (cell(x, y) == 1)
                     m_state[y * ScreenWidth() + x] = nNeighbours == 2 || nNeighbours == 3;
@@ -60,25 +60,24 @@ public:
 private:
     int* m_output;
     int* m_state;
+    int lifeArea;
+
+    bool IsInLifeArea(int x, int y, int space)
+    {
+        int screenMiddleX = (ScreenWidth() / 2);
+        int screenMiddleY = (ScreenWidth() / 2);
+        return
+            (x > screenMiddleX - space && x < screenMiddleX + space &&
+                y > screenMiddleY - space && y < screenMiddleY + space);
+    }
 };
 
 int main()
 {
-    ProjectDugo_Game game;
+    ProjectDugo_Game game(10);
 
-    game.ConstructConsole(80, 60, 8, 8);
-    game.Start();
+    if(game.ConstructConsole(80, 60, 8, 8))
+        game.Start();
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
